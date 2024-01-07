37a3150990847c3725de427b697a78d32143919d
wang mingze <wmz@jmed.com>
2024-01-07 21:57:27 +0800

    ENH: 修改训练设置代码

--- trainSemanticSegmentation.py
+++ trainSemanticSegmentation.py
@@ -12,31 +12,10 @@ import csv
 import torch
 from nets.UNet_3Ddc2 import UNet_3D as UNet
 
-# device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-device = torch.device('cpu')
-model_str = 'Deconv_ReLu_MSE'
 
-
-def train_and_eval(args):
-    dataFolderName = "AnkleSegmentation"
-    train_data_dir = r'D:\Dataset\TibiaSegmentation\train'
-    train_heatmap_dir = r'D:\Dataset\TibiaSegmentation\label'
-    # train_data_dir = '/Data/wmz/Dataset/' + dataFolderName + '/train'
-    # train_heatmap_dir = '/Data/wmz/Dataset/' + dataFolderName + '/label'
-    eval_data = '/Data/wmz/Dataset/' + dataFolderName + '/test'
-    eval_heatmap_dir = '/Data/wmz/Dataset/' + dataFolderName + '/label'
-    saved_dir = '/Data/wmz/Dataset/' + dataFolderName + '/test_result'
-
-    train_for_Seg(train_data_dir, train_heatmap_dir, model_str, args)
-    # model_path_list = sorted(glob(os.path.join('trained_model', model_str + '*/*.pth')))
-    # print(model_path_list)
-    # eval_all_models_for_seg(eval_data, eval_heatmap_dir, model_str, model_path_list, saved_dir, postfix='')
-    # eval_all_models_for_seg(patient_eval_data, patient_eval_mask_dir, model_str, model_path_list, saved_dir, postfix='_patient')
-
-
-def train_for_Seg(train_data_dir, train_heatmap_dir, model_str, args):
+def train_for_seg(data_dir, label_dir, model_str, args):
     start_time = time.strftime("%m%d%H%M%S", time.localtime())
-    dataset = SemanticDataset(train_data_dir, train_heatmap_dir, img_scale=1.0, label_scale=1.0)
+    dataset = SemanticDataset(data_dir, label_dir, img_scale=1.0, label_scale=1.0)
     # Here , class_nums --> objet_nums in segmentation....
     net = UNet(1, args.num_class)
     net.to(device)
@@ -58,11 +37,11 @@ def export_onnx(model, input, input_names, output_names, modelname):
                       verbose=False,
                       opset_version=12,
                       input_names=input_names,
-                      output_names=output_names, dynamic_axes={'input': [2, 3, 4], 'output': [2, 3, 4]})  # , dynamic_axes={'input': [2, 3, 4], 'output': [2, 3, 4]}
+                      output_names=output_names, dynamic_axes={'input': [2, 3, 4], 'output': [2, 3, 4]})
     print("export onnx model success!")
 
 
-def pred_for_Seg(eval_data, eval_heatmap_dir, model_path, model_str, class_nums):
+def pred_for_seg(eval_data, model_path, model_str, class_nums):
     start_time = time.strftime("%m%d%H%M%S", time.localtime())
     dataset = SemanticDataset(eval_data, None, 1.0, 1.0, trans_flag=False)
     net = UNet(1, class_nums)
@@ -74,20 +53,15 @@ def pred_for_Seg(eval_data, eval_heatmap_dir, model_path, model_str, class_nums)
         input_names = ['input']
         output_names = ['output']
         export_onnx(net, input, input_names, output_names, "FemurSeg.onnx")
-        # save_trace(net, input, "Hip_Femur_Locate.pt")
-        # save_script(net, "Hip_Femur_Locate.pt")
-
         exit(0)
-
     bz = 1
-    # seg_eval(net, dataset, model_str, bz, eval_heatmap_dir, saved_flag=True, eval_img_scale=1.0)
     seg_pred(net, dataset, model_str, bz, saved_flag=True, eval_img_scale=1.0)
     end_time = time.strftime("%m%d%H%M%S", time.localtime())
     print(model_str, '\tStart Time', start_time, '\tEnd Time: ', end_time)
 
 
-def eval_for_Seg(eval_data, eval_heatmap_dir, model_path, saved_dir, model_str, class_nums, saved_flag=True):
-    dataset =SemanticDataset(eval_data, eval_heatmap_dir, 1.0, 1.0, trans_flag=False)
+def eval_for_seg(eval_data, eval_heatmap_dir, model_path, saved_dir, model_str, class_nums, saved_flag=True):
+    dataset = SemanticDataset(eval_data, eval_heatmap_dir, 1.0, 1.0, trans_flag=False)
     net = UNet(1, class_nums)
     net.to(device)
     net.load_state_dict(torch.load(model_path, map_location=device))
@@ -107,7 +81,7 @@ def eval_all_models_for_seg(eval_data, eval_heatmap_dir, model_str, model_path_l
     best_info = {"model": None, "dice": None}
     for model_path in model_path_list:
         result_saved_dir = get_wanted_path(model_path, saved_dir)
-        avg = eval_for_Seg(eval_data, eval_heatmap_dir, model_path, saved_dir=result_saved_dir, model_str=model_str, saved_flag=True)
+        avg = eval_for_seg(eval_data, eval_heatmap_dir, model_path, saved_dir=result_saved_dir, model_str=model_str, saved_flag=True)
         if(max_avg < avg):
             max_avg = avg
             best_info['model'] = model_path
@@ -121,7 +95,7 @@ def eval_all_models_for_seg(eval_data, eval_heatmap_dir, model_str, model_path_l
         model_path = best_info['model']
         import shutil
         shutil.copy(model_path, os.path.join(result_saved_dir, os.path.basename(model_path)))
-        eval_for_Seg(eval_data, eval_heatmap_dir, model_path, saved_dir=result_saved_dir, model_str=model_str, saved_flag=True)
+        eval_for_seg(eval_data, eval_heatmap_dir, model_path, saved_dir=result_saved_dir, model_str=model_str, saved_flag=True)
 
     run_best_save_result()
 
@@ -142,6 +116,21 @@ def get_args():
 
 
 if __name__ == "__main__":
+    # device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
+    model_str = 'Deconv_ReLu_MSE'
     args = get_args()
-    train_and_eval(args)
+    data_folder_name = "AnkleSegmentation"
+    folder_prev = '/Data/wmz/Dataset/'
+    train_data_dir = folder_prev + data_folder_name + "/train"
+    train_heatmap_dir = folder_prev + data_folder_name + "label"
+    # 训练
+    train_for_seg(train_data_dir, train_heatmap_dir, model_str, args)
+    # 评估
+    # eval_data = folder_prev + data_folder_name + '/test'
+    # eval_heatmap_dir = folder_prev + data_folder_name + '/label'
+    # saved_dir = folder_prev + data_folder_name + '/test_result'
+    # model_path_list = sorted(glob(os.path.join('trained_model', model_str + '*/*.pth')))
+    # eval_all_models_for_seg(eval_data, eval_heatmap_dir, model_str, model_path_list, saved_dir, postfix='')
+
 
